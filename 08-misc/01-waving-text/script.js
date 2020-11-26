5a4cc03224365bf51a428f168fb595351650e83c
René Vandamme <vandammerene9+gitkraken@gmail.com>
2020-11-26 20:02:23 +0100

    Added some comments.

--- 08-misc/01-waving-text/script.js
+++ 08-misc/01-waving-text/script.js
@@ -15,19 +15,21 @@
 
   const calculateFontSize = (i, range) => {
     //Math.sin calculates sin based on the radials entered. sin(PI/2) == 1
-    //Abs makes sure i's always positive
-    // Ceil to round up.
+    //Multiply pi/2 (which sin would return as 1) by i/range (this will give values of  -1 to 1 to -1)
+    //I use abs to change the range to 0 to 1 and back to 0. Then multiply that by the range
     return Math.round(Math.abs(Math.sin((i / range) * (Math.PI / 2)) * range));
   };
 
   let offset = 0;
+  const updateInterval = 50; //in miliseconds, lower equals faster animation
   setInterval(() => {
     let letters = targetValue.split('');
     for (let i = 0; i < letters.length; i++) {
+      //Limit font size to 6 as it all fits nicely on one line this way.
       let size = calculateFontSize(i + offset, 6);
       letters[i] = letters[i].fontsize(size);
     }
     offset += 1;
     document.getElementById('target').innerHTML = letters.join('');
-  }, 50);
+  }, updateInterval);
 })();
