eea842a701f2cce8b4aeccf20428abf83ed01859
René Vandamme <vandammerene9+gitkraken@gmail.com>
2020-11-26 14:41:29 +0100

    Finished ex 7 of chapter 8.

--- 08-misc/07-storage-clicker/script.js
+++ 08-misc/07-storage-clicker/script.js
@@ -9,8 +9,15 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    // your code here
-
+(function () {
+  // your code here
+  let counter = Number(localStorage.getItem('counter'));
+  counter = counter === undefined ? 0 : counter;
+  let targetElement = document.getElementById('target');
+  targetElement.textContent = counter;
+  document.getElementById('increment').addEventListener('click', () => {
+    counter++;
+    targetElement.textContent = counter;
+    localStorage.setItem('counter', counter);
+  });
 })();
