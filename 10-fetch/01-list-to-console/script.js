bca5a33f0f00efa3654eca2cad404c584b8d8343
René Vandamme <vandammerene9+gitkraken@gmail.com>
2020-11-30 16:52:23 +0100

    Finished ex 2

--- 10-fetch/01-list-to-console/script.js
+++ 10-fetch/01-list-to-console/script.js
@@ -10,5 +10,26 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  //Note I did not get the heroes API to work using npm run api
+  //If you get CORS request did not succeed/Not found make sure the server is live.
+  // Use these commands:
+  //npm run api
+
+  //It's always good practice to define a base url (the root url of the api).
+  const BASE_URL = 'http://127.0.0.1:3000';
+
+  const onRunButtonClicked = async () => {
+    try {
+      let response = await fetch(`${BASE_URL}/heroes`);
+      if (response.ok) {
+        response.json().then((jsonData) => {
+          console.log(jsonData);
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  document.getElementById('run').addEventListener('click', onRunButtonClicked);
 })();
--- 10-fetch/02-list-to-template/script.js
+++ 10-fetch/02-list-to-template/script.js
@@ -10,5 +10,30 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  const BASE_URL = 'http://127.0.0.1:3000';
+
+  const onRunButtonClicked = async () => {
+    //first get the template
+    let template = document.getElementById('tpl-hero');
+    let targetElement = document.getElementById('target');
+
+    try {
+      let response = await fetch(`${BASE_URL}/heroes`);
+      response.json().then((jsonData) => {
+        jsonData.forEach((hero) => {
+          var heroElement = template.content.cloneNode(true);
+          heroElement.querySelector('.name').innerHTML = hero.name;
+          heroElement.querySelector('.alter-ego').innerHTML = hero.alterEgo;
+          heroElement.querySelector('.powers').innerHTML = hero.abilities.join(
+            ', '
+          );
+          targetElement.appendChild(heroElement);
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  document.getElementById('run').addEventListener('click', onRunButtonClicked);
 })();
